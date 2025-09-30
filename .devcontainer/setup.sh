73dd615f4327006a3ec8594eabddbe2bf7dd404e
Joan Gautreaux <joan@arista.com>
2025-09-30 15:09:56 +0000

    Add anta cli to lab.

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 bash -c "$(curl -sL https://get.containerlab.dev)" -- -v 0.70.2
-pip3 install passlib
+pip3 install passlib 'anta[cli]'
 ssh-keygen -t ecdsa -f ~/.ssh/id_ecdsa -q -N ""
 ansible-galaxy collection install ansible.posix
 sudo usermod -aG clab_admins $USER && newgrp clab_admins
