a4b1e0cdf80b61f81827f73f7bc716c5f4728927
Joan Gautreaux <joan@arista.com>
2026-02-08 21:52:15 +0100

    v1.1: Add support for WiFi VXLAN gateway to site1 topology and several minor improvements.
    - Add support for wifi vxlan gateways to site1 topology.
    - Add wifi vxlan gateways to clab topology.
    - Add vxlan configuration scripts for s1-host1 and s1-host2.
    - Update yaml callback plugin in ansible.cfg.
    - Add ansible and python requirements to project.
    - Update ceos image to 4.35.1F in clab topology file.
    - Update global variables and playbook files.

---
+++ clab/config/linux/s1-host2-vxlan.sh
@@ -0,0 +1,21 @@
+# Create VXLAN devices (learning ON by default)
+for vni in 20100 20101 20102 20103; do
+  ip link add wifi_vx${vni} type vxlan id ${vni} dev bond0 dstport 4789
+done
+
+# Head-end replication flood lists:
+# Add one wildcard (00:..:00) entry PER remote VTEP PER VNI.
+for vni in 20100 20101 20102 20103; do
+  bridge fdb add 00:00:00:00:00:00 dst 10.255.255.1 dev wifi_vx${vni} self permanent
+done
+
+# Assign IPs to VXLAN interfaces
+ip addr add 10.100.0.11/22 dev wifi_vx20100
+ip addr add 10.100.4.11/22 dev wifi_vx20101
+ip addr add 10.100.8.11/22 dev wifi_vx20102
+ip addr add 10.100.12.11/22 dev wifi_vx20103
+
+# Bring VXLAN interfaces up
+for vni in 20100 20101 20102 20103; do
+  ip link set wifi_vx${vni} up
+done
